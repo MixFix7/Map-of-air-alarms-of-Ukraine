b02ce0d2fe6f65f88280f1f7add89e1c00e1730e
MixFix7 <mashtalar95@gmail.com>
2023-06-07 14:50:01 +0300

    update ajax

--- ukraine_alarm_map/static/ajax/update-status-alarms.js
+++ ukraine_alarm_map/static/ajax/update-status-alarms.js
@@ -1,5 +1,15 @@
-var template = document.getElementById('alarms').innerHTML;
-var element = document
+// var template = document.getElementById('alarms');
+
+// var element = document.getElementById('div-alarms');
+
+// function renderTemplate(template, data) {
+//     var renderedTemplate = Mustache.render(template, data);
+//     return renderedTemplate;
+// };
+
+// function updateContent(element, content) {
+//     element.innerHTML = content;
+// };
 
 function get_alarms() {
 
@@ -10,8 +20,16 @@ function get_alarms() {
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status <= 400) {
             // Success server answer processing
-            var responce = JSON.parse(xhr.responseText);     
-            console.log(responce.all_alarms);
+            var data = JSON.parse(xhr.responseText);     
+            console.log(data.all_alarms)
+            
+            
+            // var alarms = data.all_alarms;
+
+            // var renderedTemplate = renderTemplate(template, alarms);
+
+            // updateContent(element, renderedTemplate);
+
         } else {
             console.error(xhr.statusText)
         }
@@ -25,10 +43,11 @@ function get_alarms() {
 
 };
 
+get_alarms()
 
-get_alarms();
 
 setInterval(get_alarms(), 15000);
+
 
 
 
